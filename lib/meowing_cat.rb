d90ac2cb6456de3c6efc8c972844ecd603aad3fc
Vincent Tran <vincentuong@yahoo.com>
2021-05-31 03:22:58 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,23 +1,5 @@
 ## code your solution here. 
 
 def Cat
-  
-  def name=(name)
-    @name = name
-  end
- 
-  def name
-    @name
-  end
-  
-  def attr_accessor=(name)
-    @name = name
-  end 
-  
-  def attr
-  
+
 end 
-
-
-amy = Cat.new
-maru.name = "Cat"
